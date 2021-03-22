de4dc86b581fb258aef1458937f1e728757b6d17
Keith Dahlby <dahlbyk@gmail.com>
2021-03-22 11:11:06 -0500

    ConsoleApp1

---
+++ class-01/demo/ConsoleApp1/Data/Stuff.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1.Data
+{
+    class Stuff
+    {
+        public static void Test(string name)
+        {
+            Console.WriteLine("Testing from Stuff!");
+            Console.WriteLine("hello, " + name);
+        }
+    }
+}
+
+// Don't to this (but it is possible)
+namespace ConsoleApp1.Stupid
+{
+    class Stuff
+    {
+        public static void Test2() { }
+    }
+
+    class Array
+    {
+
+    }
+}
---
+++ class-01/demo/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+using ConsoleApp1.Data;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array array = new Array();
+
+            Console.WriteLine("Hello World!");
+
+            // Stuff stuff = new Stuff();
+
+            //object num = 123;
+            //Stuff.Test((string)num); // Compiler error - can't convert int to string
+
+            Stuff.Test("123");
+            Stuff.Test(123.ToString());
+        }
+    }
+}
