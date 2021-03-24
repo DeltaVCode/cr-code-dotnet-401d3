e0e7b1ea01c2e8f57bf9403312dd5f8e63f0935e
Keith Dahlby <dahlbyk@gmail.com>
2021-03-24 12:03:59 -0500

    Add some File IO tests

--- class-03/demo/DemoApp.Tests/UnitTest1.cs
+++ class-03/demo/DemoApp.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace DemoApp.Tests
@@ -8,7 +9,29 @@ namespace DemoApp.Tests
         [Fact]
         public void Test1()
         {
+            // Arrange
+            // string filePathToWriteTo = $"test.txt";
+            // File.Delete(filePathToWriteTo);
 
+            // Good idea to make test input/output paths unique
+            string filePathToWriteTo = $"test-{DateTime.Now.Ticks}.txt";
+
+            string textToWrite = "Hello!";
+
+            // Act
+            File.AppendAllText(filePathToWriteTo, textToWrite);
+
+            // Assert
+            string written = File.ReadAllText(filePathToWriteTo);
+            Assert.Equal(textToWrite, written);
+        }
+
+        [Fact]
+        public void CanReadNames()
+        {
+            string[] names = File.ReadAllLines("names.txt");
+
+            Assert.Equal(new[] { "Keith", "Stacey", "Craig" }, names);
         }
     }
 }
