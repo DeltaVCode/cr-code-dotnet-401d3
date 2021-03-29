f659b388a407d67886f268cde89f520dc9bdbadc
Keith Dahlby <dahlbyk@gmail.com>
2021-03-29 11:32:34 -0500

    Party time!

--- class-06/demo/PartyTime/Program.cs
+++ class-06/demo/PartyTime/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PartyTime.Parties;
 
 namespace PartyTime
 {
@@ -6,7 +7,38 @@ namespace PartyTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Party Time!");
+
+            Party party = new PoolParty();
+            party.Setup();
+            Console.WriteLine($"Max Guests: {party.NumberOfGuests}");
+            // party.PoolDepth // error because Party doesn't have this property
+            party.Teardown();
+
+            PoolParty poolParty = new PoolParty();
+            poolParty.Setup();
+            poolParty.PoolDepth = 7;
+            Console.WriteLine($"Pool Depth: {poolParty.PoolDepth}");
+            poolParty.Teardown();
+
+
+            BirthdayParty[] bdayParties = new BirthdayParty[]
+            {
+                new KidBirthdayParty("John") { NumberOfGuests = 10 },
+                new BarBirthdayParty { NumberOfGuests = 15 },
+                new KidBirthdayParty("Paul") { NumberOfGuests = 20 },
+            };
+
+            for (int i = 0; i < bdayParties.Length; i++)
+            {
+                BirthdayParty bdayParty = bdayParties[i];
+
+                bdayParty.Setup();
+                Console.WriteLine($"Max Guests: {bdayParty.NumberOfGuests}");
+                Console.WriteLine($"Gifts? {bdayParty.AcceptGifts}");
+
+                bdayParty.Teardown();
+            }
         }
     }
 }
