1a1753b3fb5855515e10194d797ce60c7afa1a46
Keith Dahlby <dahlbyk@gmail.com>
2021-04-01 11:59:57 -0500

    How to parse JSON

---
+++ class-09/demo/Collections/Demo.Tests/Lab09GeoJsonTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class Lab09GeoJsonTests
+    {
+        [Fact]
+        public void Can_read_JSON_from_file()
+        {
+            // Arrange
+            string filename = "data.json";
+            string data = File.ReadAllText(filename);
+            Assert.NotEmpty(data);
+
+            // Assert
+            RootObject root = JsonConvert.DeserializeObject<RootObject>(data);
+        }
+
+    }
+
+    class RootObject
+    {
+        public string type { get; set; }
+
+        public List<Feature> features { get; set; }
+    }
+
+    class Feature
+    {
+        public string type { get; set; }
+
+        // TODO: geomertries, properties
+    }
+}
