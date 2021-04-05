369f45ba4ad6cab5a1d1820160d81c215481bf74
Keith Dahlby <keith@newbo.co>
2021-04-05 09:46:09 -0500

    Demo more JSON parsing stuff

--- class-09/demo/Collections/Demo.Tests/Lab09GeoJsonTests.cs
+++ class-09/demo/Collections/Demo.Tests/Lab09GeoJsonTests.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Demo.Tests
@@ -18,11 +20,42 @@ namespace Demo.Tests
             // Act
             RootObject root = JsonConvert.DeserializeObject<RootObject>(data);
 
+            JObject.Parse(data);
+
             // Assert
             Assert.Equal("FeatureCollection", root.type);
             Assert.NotEmpty(root.features);
+
+            int neighborhoodCount = root.features
+                .Select(f => f.properties.neighborhood)
+                .Distinct()
+                .Count();
+
+            Assert.Equal(40, neighborhoodCount);
         }
 
+        [Fact]
+        public void Can_read_JSON_from_file_with_JObject()
+        {
+            // Arrange
+            string filename = "data.json";
+            string data = File.ReadAllText(filename);
+            Assert.NotEmpty(data);
+
+            // Act
+            JObject root = JObject.Parse(data);
+
+            // Assert
+            Assert.Equal("FeatureCollection", root["type"]);
+            Assert.NotEmpty(root["features"]);
+
+            int neighborhoodCount = root["features"]
+                .Select(f => f["properties"]["neighborhood"])
+                .Distinct()
+                .Count();
+
+            Assert.Equal(40, neighborhoodCount);
+        }
     }
 
     class RootObject
@@ -36,6 +69,22 @@ namespace Demo.Tests
     {
         public string type { get; set; }
 
-        // TODO: geomertries, properties
+        public Geometry geometry { get; set; }
+
+        public FeatureProperties properties { get; set; }
+    }
+
+    public class FeatureProperties
+    {
+        public string zip { get; set; }
+        public string city { get; set; }
+        public string neighborhood { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+
+        public double[] coordinates { get; set; }
     }
 }
