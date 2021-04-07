06f75b98c68340a5a7a2476f0ade076228139434
Keith Dahlby <keith@newbo.co>
2021-04-07 09:11:07 -0500

    Copy Demo 12 forward

---
+++ class-13/demo/DemoWeb/Migrations/20210406164804_SeedMoreTechnologies.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoWeb.Migrations
+{
+    public partial class SeedMoreTechnologies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Technologies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "JavaScript");
+
+            migrationBuilder.InsertData(
+                table: "Technologies",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Java" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Technologies",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "Technologies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Node.js");
+        }
+    }
+}
---
+++ class-13/demo/DemoWeb/Startup.cs
@@ -0,0 +1,65 @@
+using DemoWeb.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWeb
+{
+    public class Startup
+    {
+        // 1. Place to store config
+        public IConfiguration Configuration { get; }
+
+        // 2. Populate config via magic
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Make sure controllers have what they need
+            services.AddControllers();
+
+            services.AddDbContext<SchoolDbContext>(options => {
+                // Our DATABASE_URL from js days
+                string connectionString = Configuration.GetConnectionString("DefaultConnection");
+                if (connectionString == null)
+                    throw new InvalidOperationException("Connection string is not set.");
+                options.UseSqlServer(connectionString);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Make sure controllers work
+                endpoints.MapControllers();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync($"Hello World from path {context.Request.Path}!");
+                });
+            });
+        }
+    }
+}
