dee94196e62171c8ff4d83f19ae46381925853df
Keith Dahlby <keith@newbo.co>
2021-04-08 10:30:47 -0500

    Add DbContext dependency to DatabaseCourseRepository

--- class-14/demo/DemoWeb/Services/Database/DatabaseCourseRepository.cs
+++ class-14/demo/DemoWeb/Services/Database/DatabaseCourseRepository.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DemoWeb.Data;
 
 namespace DemoWeb.Services.Database
 {
     public class DatabaseCourseRepository : ICourseRepository
     {
+        private readonly SchoolDbContext _context;
+
+        public DatabaseCourseRepository(SchoolDbContext context)
+        {
+            _context = context;
+        }
+
     }
 }
