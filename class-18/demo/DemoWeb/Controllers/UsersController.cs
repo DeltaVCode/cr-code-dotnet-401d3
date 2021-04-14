fdd43d5622e2c880c556eb35cd22fb93e0a29e38
Keith Dahlby <keith@newbo.co>
2021-04-14 09:48:48 -0500

    Add /api/Users/Register route

---
+++ class-18/demo/DemoWeb/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoWeb.Models.Api;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(RegisterData data)
+        {
+            return Ok();
+        }
+    }
+}
