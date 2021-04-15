e10a75fe22f9071712eb995508fe889363e9c18e
Keith Dahlby <keith@newbo.co>
2021-04-15 09:38:44 -0500

    Add JwtTokenService dependencies

--- class-19/demo/DemoWeb/Services/JwtTokenService.cs
+++ class-19/demo/DemoWeb/Services/JwtTokenService.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DemoWeb.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace DemoWeb.Services
 {
     public class JwtTokenService
     {
+        private readonly IConfiguration configuration;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public JwtTokenService(IConfiguration configuration, SignInManager<ApplicationUser> signInManager)
+        {
+            this.configuration = configuration;
+            this.signInManager = signInManager;
+        }
+
         public async Task<string> GetToken(ApplicationUser user, TimeSpan expiresIn)
         {
             return "token!";
