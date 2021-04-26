a75f292c394d9766fc0b24b9c452ad9ee4c12753
Keith Dahlby <keith@newbo.co>
2021-04-26 12:22:43 -0500

    Scaffold Students Index view

--- class-26/demo/WebApplication1/Controllers/StudentsController.cs
+++ class-26/demo/WebApplication1/Controllers/StudentsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -12,7 +13,21 @@ namespace WebApplication1.Controllers
         // GET: StudentsController
         public ActionResult Index()
         {
-            return View();
+            var students = new[]{
+                new Student
+                {
+                    Id = 1,
+                    FirstName = "Keith",
+                    LastName = "Dahlby",
+                },
+                new Student
+                {
+                    Id = 1,
+                    FirstName = "Stacey",
+                    LastName = "Teltser",
+                },
+            };
+            return View(students);
         }
 
         // GET: StudentsController/Details/5
