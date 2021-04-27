8606719085442a12f12de8373fb97d883c43b332
Keith Dahlby <keith@newbo.co>
2021-04-27 10:01:48 -0500

    Register can receive data and redirect to Welcome

--- class-27/demo/WebApplication1/Controllers/AccountController.cs
+++ class-27/demo/WebApplication1/Controllers/AccountController.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Identity;
 
 namespace WebApplication1.Controllers
 {
     public class AccountController : Controller
     {
         public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterData data)
+        {
+            return RedirectToAction(nameof(Welcome));
+        }
+
+
+        [HttpGet("Welcome")]
+        public IActionResult Welcome()
         {
             return View();
         }
