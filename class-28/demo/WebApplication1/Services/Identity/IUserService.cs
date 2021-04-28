f17e93d231746b46a209741c2b3aeba66ed51e95
Keith Dahlby <keith@newbo.co>
2021-04-28 08:54:51 -0500

    Copy demo 27 forward

---
+++ class-28/demo/WebApplication1/Services/Identity/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebApplication1.Models.Identity;
+
+namespace WebApplication1.Services.Identity
+{
+    public interface IUserService
+    {
+        Task<ApplicationUser> Register(RegisterData data, ModelStateDictionary modelState);
+
+        Task<bool> SignIn(LoginData data);
+    }
+}
