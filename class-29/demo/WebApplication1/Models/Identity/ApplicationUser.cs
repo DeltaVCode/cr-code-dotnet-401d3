2577efcfd79b567ee929baa4d51b7a8bbd0c2967
Keith Dahlby <keith@newbo.co>
2021-04-29 10:38:27 -0500

    Add ProfileImageUrl to AppUser

--- class-29/demo/WebApplication1/Models/Identity/ApplicationUser.cs
+++ class-29/demo/WebApplication1/Models/Identity/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -8,5 +9,7 @@ namespace WebApplication1.Models.Identity
 {
     public class ApplicationUser : IdentityUser<int>
     {
+        [StringLength(250)]
+        public string ProfileImageUrl { get; set; }
     }
 }
