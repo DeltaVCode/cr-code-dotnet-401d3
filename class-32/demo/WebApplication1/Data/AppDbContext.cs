9219fc73fc65da15ed63ecbdd793c56b423c8288
Keith Dahlby <keith@newbo.co>
2021-05-04 09:00:44 -0500

    Init Demo 32

---
+++ class-32/demo/WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models.Identity;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class AppDbContext :
+        IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // Keep this so Identity still works!
+            base.OnModelCreating(builder);
+
+            builder.Entity<Course>()
+                .HasData(
+                new Course { Id = 42, CourseCode = "Life", Price = 123.45m }
+                );
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
