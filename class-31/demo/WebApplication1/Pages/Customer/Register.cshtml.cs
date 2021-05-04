eaafadd99e1199f1ae73f4eee8649936becf86e1
Keith Dahlby <keith@newbo.co>
2021-05-04 10:38:26 -0500

    Add basic Register as Razor Page

---
+++ class-31/demo/WebApplication1/Pages/Customer/Register.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models.Identity;
+using WebApplication1.Services.Identity;
+
+namespace WebApplication1.Pages.Customer
+{
+    public class RegisterModel : PageModel
+    {
+        private readonly IUserService userService;
+
+        public RegisterModel(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [BindProperty]
+        public RegisterData Input { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page(); // similar to return View() in Controller
+            }
+
+            await userService.Register(Input, ModelState);
+
+            // Might have duplicate email, invalid password, etc
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return Redirect("~/");
+        }
+    }
+}
