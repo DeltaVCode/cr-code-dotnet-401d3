116942f5f4341d7f5d24ba14fb55b0b3fd613747
Keith Dahlby <keith@newbo.co>
2021-05-04 09:51:15 -0500

    Add useless Username ViewComponent

---
+++ class-32/Demo/TodoApi/Components/UsernameComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Services;
+
+namespace TodoApi.Components
+{
+    public class Username : ViewComponent
+    {
+        readonly IUserService userService;
+
+        public Username(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int number)
+        {
+            var user = await userService.GetUser(this.UserClaimsPrincipal);
+            return View(user);
+        }
+    }
+}
