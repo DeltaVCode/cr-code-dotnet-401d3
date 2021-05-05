9bfd4eb419ce80fe1e208f51dbaf2459eb9c6419
Keith Dahlby <keith@newbo.co>
2021-05-05 10:50:30 -0500

    Add some SendGrid services we're not going to use

---
+++ class-33/Demo/WebApplication1/Services/SendGridEmailService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication1.Services
+{
+    public class SendGridHttpEmailService : IEmailService
+    {
+        public IConfiguration configuration;
+
+        public SendGridHttpEmailService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public Task SendEmail(string email, string subject, string body)
+        {
+            var http = new HttpClient();
+            var sendUrl = configuration["SendGrid:ApiSendUrl"] ?? throw new InvalidOperationException("Missing ApiSendUrl");
+
+            // http.PostAsync(sendUrl, )
+            throw new NotSupportedException("This is too hard");
+        }
+    }
+
+    public class SendGridSmtpEmailService : IEmailService
+    {
+        public Task SendEmail(string email, string subject, string body)
+        {
+            var smtp = new SmtpClient();
+            // ...
+            return Task.CompletedTask;
+        }
+    }
+}
