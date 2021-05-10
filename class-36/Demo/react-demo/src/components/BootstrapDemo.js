d30341d5f9d0def97ff30d08ed57df4469cba1b8
Keith Dahlby <keith@newbo.co>
2021-05-10 11:57:50 -0500

    Demo some React Bootstrap!

--- class-36/Demo/react-demo/src/components/BootstrapDemo.js
+++ class-36/Demo/react-demo/src/components/BootstrapDemo.js
@@ -1,6 +1,15 @@
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+
 export default function BootstrapDemo()
 {
+    // <> is a React.Fragment
     return (
-        <h1>Bootstrap demo goes here!</h1>
+        <>
+            <h1>Bootstrap demo goes here!</h1>
+            <Alert variant="danger">Our first alert!</Alert>
+
+            <Button variant="danger">Hi!</Button>
+        </>
     )
 }
