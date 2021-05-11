003bffd2ea2db2211e29400584acfdc4139c8dbc
Keith Dahlby <keith@newbo.co>
2021-05-11 11:12:30 -0500

    Counter does not work

--- class-37/Demo/react-demo/src/components/FormDemo.js
+++ class-37/Demo/react-demo/src/components/FormDemo.js
@@ -1,4 +1,31 @@
-export default function FormUncontrolledDemo(){
+
+export default function FormDemo(){
+    return (
+        <>
+            <Counter />
+            <FormUncontrolledDemo />
+        </>
+    )
+}
+
+function Counter()
+{
+    let num = 5;
+    const increment = (event) => {
+        num++;
+        console.log(num);
+    }
+
+    return (
+        <form>
+            <h3>Counter Value: {num}</h3>
+            <input type="text" value={num} />
+            <button type='button' onClick={increment}>+1</button>
+        </form>
+    )
+}
+
+function FormUncontrolledDemo(){
     const handleSubmit = e => {
         e.preventDefault();
 
@@ -10,6 +37,7 @@ export default function FormUncontrolledDemo(){
 
     return (
         <form onSubmit={handleSubmit}>
+            <h3>Uncontrolled!</h3>
             <input type="text" name="word" />
         </form>
     )
