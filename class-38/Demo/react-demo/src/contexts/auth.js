f146561e0f7dab36e54877747c55f3b3ed4ffdc2
Keith Dahlby <keith@newbo.co>
2021-05-12 11:34:23 -0500

    Add AuthProvider

---
+++ class-38/Demo/react-demo/src/contexts/auth.js
@@ -0,0 +1,15 @@
+import { useContext, useState } from 'react';
+
+export const AuthContext = React.createContext();
+
+export function AuthProvider(props) {
+    const [state, setState] = useState({
+        user: { name: 'Keith' },
+    });
+
+    return (
+        <AuthContext.Provider value={state}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
