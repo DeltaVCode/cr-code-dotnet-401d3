1e5d68d73c28670540261bbec1d069e4988a50b8
Keith Dahlby <keith@newbo.co>
2021-05-12 11:26:24 -0500

    Add NavLogin and other uses of user

--- class-38/Demo/react-demo/src/App.js
+++ class-38/Demo/react-demo/src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import BootstrapDemo from './components/BootstrapDemo';
 import FormDemo from './components/FormDemo'
+import NavLogin from './components/NavLogin';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({ name: 'Keith' });
+
   return (
     <div className="App">
       <nav>
@@ -12,6 +16,7 @@ function App() {
           <li><NavLink to="/" exact>Home</NavLink></li>
           <li><NavLink to="/forms">Form Demo</NavLink></li>
           <li><NavLink to="/bootstrap">Bootstrap</NavLink></li>
+          <NavLogin user={user} />
         </ul>
       </nav>
       <main>
@@ -20,7 +25,7 @@ function App() {
             <Home />
           </Route>
           <Route path="/bootstrap">
-            <BootstrapDemo />
+            <BootstrapDemo user={user} />
           </Route>
           <Route path="/forms">
             <FormDemo />
--- class-38/Demo/react-demo/src/components/BootstrapDemo.js
+++ class-38/Demo/react-demo/src/components/BootstrapDemo.js
@@ -1,13 +1,15 @@
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 
-export default function BootstrapDemo()
+export default function BootstrapDemo(props)
 {
+    const { user } = props;
+
     // <> is a React.Fragment
     return (
         <>
             <h1>Bootstrap demo goes here!</h1>
-            <Alert variant="danger">Our first alert!</Alert>
+            <Alert variant="danger">Our first alert, {user.name}!</Alert>
 
             <Button variant="danger">Hi!</Button>
         </>
