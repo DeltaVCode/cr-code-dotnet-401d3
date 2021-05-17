b633cd541bdbd1e404c068ba7dc60643f82d8220
Keith Dahlby <keith@newbo.co>
2021-05-17 12:51:43 -0500

    Expo demo

---
+++ class-41/Demo/my-first-expo-app/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
+
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+
+        setContacts(data);
+      }
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize : 100 }}>Hello from DeltaV!</Text>
+      {contacts.map((c,i) => (
+        <Text key={i}>{c.lookupKey}</Text>
+      ))}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
