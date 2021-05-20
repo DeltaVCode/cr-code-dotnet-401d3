3ba4c9b470218f538c5ca04f8fe30cf4e4d2f3aa
Keith Dahlby <keith@newbo.co>
2021-05-20 11:54:15 -0500

    Add a database test

---
+++ class-44/demo/DemoWeb.Tests/Data/DatabaseStudentRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoWeb.Data;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace DemoWeb.Tests.Data
+{
+    public class DatabaseStudentRepositoryTests : DatabaseTestBase
+    {
+        [Fact]
+        public async Task Can_get_Students()
+        {
+            // Arrange
+            // var doug = await _db.Students.SingleAsync(); // Dougie Fresh
+            var s1 = await CreateAndSaveTestStudent();
+            var s2 = await CreateAndSaveTestStudent();
+
+            var studentRepo = new DatabaseStudentRepository(_db);
+
+            // Act
+            var students = await studentRepo.GetAllStudents();
+
+            // Assert
+            Assert.Contains(s1, students);
+            Assert.Contains(s2, students);
+        }
+    }
+}
