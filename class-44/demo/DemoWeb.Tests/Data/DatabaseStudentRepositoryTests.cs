bcdfc9c898433d443d3b0ebfed772a280eda9c7a
Keith Dahlby <keith@newbo.co>
2021-05-20 12:03:41 -0500

    Add a failing test for Add Student

--- class-44/demo/DemoWeb.Tests/Data/DatabaseStudentRepositoryTests.cs
+++ class-44/demo/DemoWeb.Tests/Data/DatabaseStudentRepositoryTests.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DemoWeb.Data;
+using DemoWeb.Models;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
@@ -27,6 +28,25 @@ namespace DemoWeb.Tests.Data
             // Assert
             Assert.Contains(s1, students);
             Assert.Contains(s2, students);
+        }
+
+        [Fact]
+        public async Task Can_add_student()
+        {
+            // Arrange
+            var saveMe = new Student { FirstName = "Bob", LastName = "Barker" };
+
+            var studentRepo = new DatabaseStudentRepository(_db);
+
+            // Act
+            await studentRepo.CreateStudent(saveMe);
+
+            // Assert
+            Assert.NotEqual(0, saveMe.Id);
+
+            var saved = await studentRepo.GetStudent(saveMe.Id);
+            Assert.NotNull(saved);
+            Assert.Equal(saveMe.FirstName, saved.FirstName);
         }
     }
 }
