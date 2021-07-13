f0cdd595eba52b93c9b27d0fbd2ac108bb94cba4
Keith Dahlby <dahlbyk@gmail.com>
2021-07-13 17:03:25 -0500

    Add ShouldHaveResult() helper for ActionResult<T>

---
+++ class-19/demo/DemoWeb.Tests/Controllers/ActionResultExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace DemoWeb.Tests.Controllers
+{
+    public static class ActionResultExtensions
+    {
+        public static T ShouldHaveValue<T>(this ActionResult<T> result, int? expectedStatusCode = 200)
+        {
+            if (result.Result == null)
+            {
+#pragma warning disable xUnit2000 // Constants and literals should be the expected argument
+                // ActionResult<T> with Value is treated as 200
+                Assert.Equal(expectedStatusCode, 200);
+#pragma warning restore xUnit2000 // Constants and literals should be the expected argument
+
+                return result.Value;
+            }
+
+            if (result.Result is ObjectResult objResult)
+            {
+                Assert.Equal(expectedStatusCode, objResult.StatusCode);
+
+                return Assert.IsAssignableFrom<T>(objResult.Value);
+            }
+
+            throw new ArgumentException($"Result does not have Value of type {typeof(T).Name}.");
+        }
+    }
+}
---
+++ class-19/demo/DemoWeb.Tests/Controllers/StudentsControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using DemoWeb.Controllers;
+using DemoWeb.Data;
+using DemoWeb.Models;
+using Moq;
+using Xunit;
+
+namespace DemoWeb.Tests.Controllers
+{
+    public class StudentsControllerTests
+    {
+        [Fact]
+        public async void GetStudents_returns_Ok_students()
+        {
+            // Arrange
+            var students = new List<Student> { new Student() };
+
+            var mockRepo = new Mock<IStudentRepository>();
+            mockRepo.Setup(r => r.GetAllStudents()).ReturnsAsync(students);
+
+            var controller = new StudentsController(mockRepo.Object);
+
+            // Act
+            var result = await controller.GetStudents();
+
+            // Assert
+            var value = result.ShouldHaveValue();
+
+            Assert.Equal(students, value);
+        }
+    }
+}
