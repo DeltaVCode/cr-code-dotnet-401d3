a46b8faf037e15b9eec5a78cbb3b7e1b8f9febf2
Keith Dahlby <dahlbyk@gmail.com>
2021-07-13 11:43:48 -0500

    Test TranscriptsController.Post()

--- class-19/demo/DemoWeb.Tests/Controllers/TranscriptsControllerTests.cs
+++ class-19/demo/DemoWeb.Tests/Controllers/TranscriptsControllerTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using DemoWeb.Controllers;
+using DemoWeb.Models;
 using DemoWeb.Models.Api;
 using DemoWeb.Services;
 using Moq;
@@ -34,6 +35,29 @@ namespace DemoWeb.Tests.Controllers
             Assert.NotNull(result);
             Assert.NotEmpty(result);
             Assert.Equal(transcriptEntries, result);
+        }
+
+        [Fact]
+        public async void Post_saves_student()
+        {
+            // Arrange
+            var studentId = 50;
+
+            var createTranscript = new CreateTranscript
+            {
+                CourseId = 12,
+                Grade = "B",
+            };
+
+            var repoMock = new Mock<ITranscriptRepository>();
+
+            var controller = new TranscriptsController(repoMock.Object);
+
+            // Act
+            await controller.Post(studentId, createTranscript);
+
+            // Assert
+            repoMock.Verify(r => r.AddToTranscript(studentId, createTranscript));
         }
     }
 }
